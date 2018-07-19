8c88e36aab1cd42550ef13610f4b404e5cfecda5
Zohaib <zohaib.hassan@mail.mcgill.ca>
2018-07-19 14:21:07 +0000

    Finished exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,17 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Zohaib", last_name: "Hassan", hourly_rate: 120)
+@store1.employees.create(first_name: "Eric", last_name: "Joo", hourly_rate: 100)
+@store2.employees.create(first_name: "Oli", last_name: "Martin", hourly_rate: 80)
+@store2.employees.create(first_name: "Nicholas", last_name: "something", hourly_rate: 100)
+@store3.employees.create(first_name: "ELiza", last_name: "something", hourly_rate: 120)
+@store3.employees.create(first_name: "Emma", last_name: "Virani", hourly_rate: 80)
+
+
+
+
+
+
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,4 @@
 class Store < ActiveRecord::Base
+	has_many :employees`
 
 end
